bf99802d56d95b5553e2ad28aee1ab3434135524
HNABLI <nablihatem3@gmail.com>
2026-07-08 01:32:11 +0200

    test: checking client final message computing including encodedchannelbinding, server nonce and client proof.

--- test/src/ScramTests.cpp
+++ test/src/ScramTests.cpp
@@ -181,3 +181,22 @@ TEST(SaslTests, SaslTests_Scram_CredentialsAfterEmptyServerMessage__Test) {
     EXPECT_FALSE(clientNonce.empty());
 }
 
+TEST(SaslTests, SaslTests_Scram_ExchangeAuthenticationAfterUserNameAndClientNonceSent__Test) {
+    Sasl::Client::Scram mechanism;
+    mechanism.SetHashFunction(
+        static_cast<std::vector<uint8_t> (*)(const std::vector<uint8_t>&)>(Sha1::Sha1Bytes),
+        Sha1::SHA1_BLOCK_SIZE, 160);
+    mechanism.SetCredentials("hanter2", "toto");
+    const auto userNameWithClientNonce = mechanism.ExchangeAuthentication("");
+    const auto clientNonce = userNameWithClientNonce.substr(12);
+    const auto serverNonce = clientNonce + "titi";
+    const auto base64Salt = Base64::EncodeToBase64("MySalt");
+    const auto clientFinalMessage =
+        mechanism.ExchangeAuthentication("r=" + serverNonce + ",s=" + base64Salt + ",i=4096");
+    const auto expectedClientProofAndServerSignature = ComputeClientProofServerSignature(
+        "toto", "hanter2", base64Salt, clientNonce, serverNonce, 4096,
+        static_cast<std::vector<uint8_t> (*)(const std::vector<uint8_t>&)>(Sha1::Sha1Bytes),
+        Sha1::SHA1_BLOCK_SIZE, 160);
+    EXPECT_EQ("c=biws,r=" + serverNonce + ",p=" + expectedClientProofAndServerSignature.clientProof,
+              clientFinalMessage);
+}
